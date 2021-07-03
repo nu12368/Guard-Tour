a6cbe802f651f56ca73c5069aa430ad7879aacc8
nu12368 <nu1236888@gmail.com.com>
2021-07-03 11:35:30 +0700

    first commit

---
+++ ip.js
@@ -0,0 +1,1 @@
+'https://api.guardtour.wacappcloud.com/'
---
+++ js/pages/medias/image-gallery.js
@@ -0,0 +1,12 @@
+$(function () {
+ 
+  $('.thumbnail').lightGallery({
+      selector: 'img',
+  });
+
+    // $('#a_imageview').each(function (i, v) {
+    //   $(v).lightGallery({
+    //     thumbnail: false
+    //   });
+    // });
+});
